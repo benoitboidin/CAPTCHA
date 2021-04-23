79ef6e091a43623b0a779ef2f45ed0192966e2a5
Benoît Boidin <benoit.bboidin@gmail.com>
2021-04-23 22:00:30 +0200

    update

--- verif.php
+++ verif.php
@@ -1,41 +1,41 @@
 <?php	
-	// Vérification des réponses pour le texte. 
-	if (isset($_POST['reptxt'])){
-		if ($_POST['reptxt']==$_POST['veriftxt']){
-			$restxt='1';
-		}
-		else{
-			$restxt='0';
-		}
-	}
-
-	// Vérification des réponses pour l'image.
-	if (isset($_POST['tpsimg'])){ 
-		$resimg=1;
-		$i=0;
-		foreach ($_POST['repimg'] as $ligne) {
-	    	if ($ligne!=0){
-	    		$resimg=0;
-	    		$i=+1;
-	    	}
-		}
-		if ($_POST['verifimg']!=count($_POST['repimg'])){
-			$resimg=0;
-		}
-	}
-	
-	// Vérification des réponses pour l'illusion.
-	if (isset($_POST['repill'])){
-	    $repill='';
-	    foreach ($_POST['repill'] as $ligne) {
-	        $repill = $repill.$ligne;
-	    }
-
-	    if ($repill==substr($_POST['verifill'],0,1)){
-	        $resill=1;
-	    }
-	    else{
-	        $resill=0;
-	    }
+// Vérification des réponses pour le texte.
+if (isset($_POST['reptxt'])){
+    if ($_POST['reptxt']==$_POST['veriftxt']){
+        $restxt='1';
     }
+    else{
+        $restxt='0';
+    }
+}
+
+// Vérification des réponses pour l'image.
+if (isset($_POST['tpsimg'])){
+    $resimg=1;
+    $i=0;
+    foreach ($_POST['repimg'] as $ligne) {
+        if ($ligne!=0){
+            $resimg=0;
+            $i=+1;
+        }
+    }
+    if ($_POST['verifimg']!=count($_POST['repimg'])){
+        $resimg=0;
+    }
+}
+
+// Vérification des réponses pour l'illusion.
+if (isset($_POST['repill'])){
+    $repill='';
+    foreach ($_POST['repill'] as $ligne) {
+        $repill = $repill.$ligne;
+    }
+
+    if ($repill==substr($_POST['verifill'],0,1)){
+        $resill=1;
+    }
+    else{
+        $resill=0;
+    }
+}
 ?>
--- verif_img.py
+++ verif_img.py
@@ -1,14 +1,16 @@
+# Importation des librairies utiles.
 from os import listdir
 from os.path import isfile, join
 
+# Ouverture du dossier des images.
 names = [f for f in listdir("/Users/benoitboidin/Desktop/Site mémoire/sample_img")
          if isfile(join("/Users/benoitboidin/Desktop/Site mémoire/sample_img", f))]
 orign = []
-
 for i in range(len(names)):
     orign.append(names[i][0:9])
+    
+# Création d'un fichier pour lister les noms.
 fichier = open("/Users/benoitboidin/Desktop/Site mémoire/verif_img.txt", "w", encoding='utf8')
-
 for i in range(len(orign)):
     print(orign[i])
     fichier.write(orign[i] + "\n")
